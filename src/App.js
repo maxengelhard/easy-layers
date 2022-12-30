df1d3c6dc782384733b0f71ce4b3b7a1546ccacf
MaxVEngelhard <maxvengelhard@gmail.com>
2022-12-29 21:16:25 -0600

    Reordered App for side bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import './90s.css';
 import React from 'react'
 import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+/*css */
+import './App.css';
 
+/* views */
+import Home from './views/Home';
+import About from './views/About';
+import ExistingLayers from './views/ExistingLayers';
 
 function App() {
   return (
-    <Header />
+   
+    <div className="App">
+       <Router>
+      <div className="sidebar">
+        <Header />
+      </div>
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/layers" element={<ExistingLayers />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+      </Router>
+    </div>
   );
 }
+
 
 export default App;
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react'
+import {Link } from 'react-router-dom';
 
-/* views */
-import Home from '../views/Home';
-import About from '../views/About';
-import ExistingLayers from '../views/ExistingLayers';
 import logo from '../logo192.png';
 
-/* componenets */
-
-
-const MobileMenu = () => {
-    const [isExpanded, setIsExpanded] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsExpanded(!isExpanded);
-    }
-
-    const HamburgerIcon = () => {
-        return (
-          <div className="hamburger-icon" onClick={() => toggleMenu()}>
-            <div className="line" />
-            <div className="line" />
-            <div className="line" />
-          </div>
-        );
-      };
-  
-    return (
-      <div className="mobile-menu">
-        <HamburgerIcon />
-        <ul className={`menu-items ${isExpanded ? 'expanded' : 'collapsed'}`}>
-          <li>Menu item 1</li>
-          <li>Menu item 2</li>
-          <li>Menu item 3</li>
-          ...
-        </ul>
-      </div>
-    );
-  };
-  
 
 const Header = () => {
     return (
-    <Router>
     <nav className='menu'>
     <Link to="/"><img src={logo} alt='logo'></img></Link>
-      <MobileMenu /> 
-      <Link to="/">Create Layer(s)</Link>
-      <Link to="/layers">Existing Layers</Link>
-      <Link to="/about">About</Link>
+      <Link className='view' to="/">Create Layer</Link>
+      <Link className='view' to="/layers">Existing Layers</Link>
+      <Link className='view' to="/about">About</Link>
     </nav>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/layers" element={<ExistingLayers />} />
-    <Route path="/about" element={<About />} />
-  </Routes>
-</Router>
 )
 }
 
