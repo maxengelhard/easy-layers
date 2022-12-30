d942eb9422e17e2a6f44f9fab04c4cbd027cc9be
MaxVEngelhard <maxvengelhard@gmail.com>
2022-12-29 21:41:50 -0600

    Edited nav bar look

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import logo from '../logo192.png';
 const Header = () => {
     return (
     <nav className='menu'>
-    <Link to="/"><img src={logo} alt='logo'></img></Link>
+    <Link className='logo' to="/"><img src={logo} alt='logo'></img></Link>
       <Link className='view' to="/">Create Layer</Link>
       <Link className='view' to="/layers">Existing Layers</Link>
       <Link className='view' to="/about">About</Link>
