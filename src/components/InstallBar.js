3a36a045bf71b0ce3f881adee04347123fb3d2aa
MaxVEngelhard <maxvengelhard@gmail.com>
2023-01-06 22:20:49 -0600

    Made layout of front page nicer

--- src/components/InstallBar.js
+++ src/components/InstallBar.js
@@ -18,7 +18,7 @@ const InstallBar = () => {
   
     return (
       <div className='layer-inputs'>
-        <form>
+        <form className='left-column'>
             <div className="input-group">
                 <label htmlFor="required-input">Layer (required):</label>
                 <input id="version" type="text" required value={text1} onChange={handleChange1} />
@@ -28,11 +28,15 @@ const InstallBar = () => {
                 <input id="version" type="text" value={text2} onChange={handleChange2}/>
             </div>
         </form>
+        <div className='right-column'>
             <div className='command-description'>
             <div>Command that will run:</div>
             <div>pip install {text2.length===0 ? text1: `${text1}===${text2}`}</div>
-            </div>
-        <button className='submit-button' onClick={handleSubmit}>Submit</button>
+            <button className='submit-button' onClick={handleSubmit}>Submit</button>
+        </div>
+      </div>
+        
+
       </div>
     );  
 };
