397845e7693c6cbd12aa8224145181d912dc1ca5
MaxVEngelhard <maxvengelhard@gmail.com>
2023-01-28 18:35:59 -0600

    Adding sidebar add

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,21 +3,25 @@ import React from 'react';
 const Footer = () => {
   return (
     <div 
-    // style={{
+    style={{
     //   position: 'fixed',
     //   bottom: 0,
     //   width: '100%',
     //   backgroundColor: '#006699',
-    //   textAlign: 'center',
-    //   padding: '10px 0px',
+      textAlign: 'center',
+      padding: '5px',
+      margin: 'auto',
+      overflowY: 'scroll',
+      
     //   height: '15vh',
     //   left: '15%',
     //   display: 'flex',
     //   justifyContent: 'center',
     //   alignItems: 'center'
-    // }}
+    }}
     >
-      <div id="amzn-assoc-ad-df2357f2-adc7-4060-8268-a6474f63b57d"></div>
+      <div style={{ height: '80vh'}} id="amzn-assoc-ad-df2357f2-adc7-4060-8268-a6474f63b57d"></div>
+      {/* <div id="amzn-assoc-ad-e5881a3e-6cb6-43e0-bb6e-4f6e4806f9e3"></div> */}
     </div>
   );
 }
