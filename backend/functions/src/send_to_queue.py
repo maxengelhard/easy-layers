d998b37a228c83ec116d37fc039fe05b72ca0376
Max Verser Engelhard <60617919+maxengelhard@users.noreply.github.com>
2023-03-13 13:29:44 -0500

    Send to Queue will now be configurable for env's

--- backend/functions/src/send_to_queue.py
+++ backend/functions/src/send_to_queue.py
@@ -1,11 +1,21 @@
 import json
 import boto3
+import platform
+import sys
+import os
 
 sqs = boto3.client('sqs')
 
 def lambda_handler(event, context):
-    queue_url = 'https://sqs.us-east-1.amazonaws.com/737802338670/my-queue'
-    print(event)
+    
+    machine = 'arm' if platform.machine() == 'aarch64' else 'x86'
+    run_time = ''.join(sys.version.split(' ')[0].split('.')[0:2])
+    queue_name = f'py-{run_time}-{machine}-sqs-queue' 
+    aws_account_id = context.invoked_function_arn.split(":")[4]
+    region = os.environ.get('AWS_REGION')
+    queue_url = f'https://sqs.{region}.amazonaws.com/{aws_account_id}/{queue_name}'
+    
+    
     message = json.dumps(event)
     response = sqs.send_message(QueueUrl=queue_url, MessageBody=message)
     return {
