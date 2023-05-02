29372edb9fd587e11077fe0bd435e7f66b62bb75
MaxVEngelhard <maxvengelhard@gmail.com>
2023-05-02 15:36:14 -0500

    Adding an api to generate a zip file for users to download

---
+++ backend/functions/src/generate_zip.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+import platform
+import sys
+import os
+from lambda_decorators import json_http_resp,cors_headers
+
+
+Lambda = boto3.client('lambda')
+s3 = boto3.client('s3')
+s3_bucket = os.environ['S3_BUCKET']
+
+@cors_headers
+@json_http_resp
+def lambda_handler(event, context):
+    """
+    Generate a pre-signed URL for an S3 object.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        object_key (str): The key of the S3 object.
+        expiration (int, optional): The expiration time of the pre-signed URL in seconds. Defaults to 3600.
+
+    Returns:
+        str: The pre-signed URL.
+    """
+    object_key = event['body']['key']
+    response = s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': s3_bucket,
+            'Key': object_key
+        },
+        ExpiresIn=3600
+    )
+
+    return response
+
+
+
+
+
+
+
